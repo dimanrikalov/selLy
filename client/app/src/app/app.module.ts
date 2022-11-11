412fd16615f300705e835ccb7e41c0244aab1426
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-11 12:05:44 +0200

    Added some global styles and created header component

--- client/app/src/app/app.module.ts
+++ client/app/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
