c2147ee1cf9bacbb32c72afbbea60a12b1c88d89
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-11 18:21:12 +0200

    Added dynamics to the navigation bar

--- client/app/src/app/core/header/header.component.ts
+++ client/app/src/app/core/header/header.component.ts
@@ -3,13 +3,28 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor() {}
 
   ngOnInit(): void {
+    this.initNavbar();
+  }
+
+  initNavbar () {
+    const navbar: any = document.querySelector('header');
+    let prevScrollPos = window.scrollY;
+    window.onscroll = function (): void {
+      let currentScrollPos = window.scrollY;
+      if (prevScrollPos > currentScrollPos) {
+        navbar.style.top = '0px';
+      } else {
+        navbar.style.top = '-100px';
+      }
+      prevScrollPos = currentScrollPos;
+    };
   }
 
 }
