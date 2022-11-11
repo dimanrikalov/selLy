68341877af0f18d03b7e047d64945da9ed152567
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-11 12:22:37 +0200

    Added html and css to the header content

--- client/app/src/app/app.module.ts
+++ client/app/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { CoreModule } from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+
 @NgModule({
   declarations: [
     AppComponent,
