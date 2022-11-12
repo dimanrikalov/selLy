653e4a7b926403330243f2554ef6c11fa6baf7da
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-12 22:02:10 +0200

    Fixed isAuthor of comment middleware

--- server/middlewares/isCommentAuthor.js
+++ server/middlewares/isCommentAuthor.js
@@ -1,7 +1,7 @@
 const commentService = require('../services/commentService');
 
 module.exports = async (req, res, next) => {
-    const comment = await commentService.getById(req.body.userId);
+    const comment = await commentService.getById(req.params.commentId);
     if (req.body.userId.toString() !== comment.userId.toString()) {
         return res.status(404).json({
             errorMessage: `You must be the comment's author in order to edit it!`,
