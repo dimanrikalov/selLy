0401f6b329ffc388acffcaa5af24cee78ae87873
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-17 14:54:45 +0200

    added operation icons logic

--- client/app/src/app/listing-details/listing-details.component.ts
+++ client/app/src/app/listing-details/listing-details.component.ts
@@ -10,12 +10,14 @@ import { ListingDetailsService } from '../services/listing-details.service';
 export class ListingDetailsComponent implements OnInit {
 
   listing: IListing | null = null;
+  loggedUserId : string | null = localStorage.getItem('userId');
 
   constructor(private listingDetailsService: ListingDetailsService) { }
 
   ngOnInit(): void {
     this.listingDetailsService.loadListing().subscribe({
       next: (listing) => {
+        localStorage.setItem('userId', '636fea4d871ff87fe625a7aa');
         console.log(listing);
         this.listing = listing;
       },
