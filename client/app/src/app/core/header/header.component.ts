5c4aab4e8e2cf925cab0d35241967677fe41effc
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-18 13:50:28 +0200

    Made welcome page responsive

--- client/app/src/app/core/header/header.component.ts
+++ client/app/src/app/core/header/header.component.ts
@@ -10,21 +10,8 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.initNavbar();
+
   }
 
-  initNavbar () {
-    const navbar: HTMLElement | null = document.querySelector('header');
-    let prevScrollPos = window.scrollY;
-    window.onscroll = function (): void {
-      let currentScrollPos = window.scrollY;
-      if (prevScrollPos > currentScrollPos) {
-        navbar!.style.top = '0px';
-      } else {
-        navbar!.style.top = '-100px';
-      }
-      prevScrollPos = currentScrollPos;
-    };
-  }
 
 }
