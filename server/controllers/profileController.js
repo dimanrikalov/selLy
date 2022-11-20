3d7278d0bad9d95fbc9cc81ac66f57b407ebb722
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-20 13:42:39 +0200

    updated profile controller

--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -4,11 +4,20 @@ const userService = require('../services/userService');
 router.post('/', async (req, res) => {
     const user = await userService.getByIdDetailed(req.body.userId);
     console.log(user);
-    if(user) {
+    if (user) {
         return res.json(user);
     }
-    
+
     return res.status(400).json({ errorMessage: 'User not found!' });
+});
+
+router.post('/saved-listings', async (req, res) => {
+    const user = await userService.getUserWithSavedListings(req.body.userId);
+    if (user) {
+        return res.json(user.savedListings);
+    }
+
+    return res.status(400).json({ errorMessage: 'User not found! ' });
 });
 
 module.exports = router;
