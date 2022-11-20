2f8f29c69193af7cdaca6fa3470ae565ff756935
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-20 13:38:01 +0200

    addded saved listings component

--- client/app/src/app/saved-listings/saved-listings.component.ts
+++ client/app/src/app/saved-listings/saved-listings.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { IListing } from '../interfaces/Listing';
+import { Component, OnInit } from '@angular/core';;
+import { SavedListingsService } from '../services/saved-listings.service';
 
 @Component({
-  selector: 'app-saved-listings',
+  selector: 'app-saved-listings-page',
   templateUrl: './saved-listings.component.html',
-  styleUrls: ['./saved-listings.component.css']
+  styleUrls: ['./saved-listings.component.css'],
 })
-export class SavedListingsComponent {
+export class SavedListingsPageComponent implements OnInit {
+  
+  savedListings: IListing[] | null = null;
+  
+  constructor(private savedListingsApi: SavedListingsService) {}
 
+  ngOnInit(): void {
+    this.savedListingsApi.loadSavedListings().subscribe({
+      next: (value) => {
+        console.log(value);
+        this.savedListings = value;
+      },
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
 }
