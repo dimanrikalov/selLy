22c7e5902405fdc77db416317f1826f095839dbe
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-23 19:15:48 +0200

    Created user-register service

---
+++ client/app/src/app/services/user-register.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+const registerUrl = environment.registerUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserRegisterService {
+  constructor(private httpClient: HttpClient) { }
+
+  register(registerInfo: {email: string, name: string, password: string, repeatPassword: string, profileImage: string}) {
+    return this.httpClient.post<{message: string, userId: string}>(registerUrl, registerInfo);
+  }
+}
