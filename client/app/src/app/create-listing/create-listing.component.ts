bb92e1167cda59097d9a1290088d0b724f8f5ac2
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-23 22:16:31 +0200

    updated create listing component

--- client/app/src/app/create-listing/create-listing.component.ts
+++ client/app/src/app/create-listing/create-listing.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CreateListingService } from '../services/create-listing.service';
 
 @Component({
   selector: 'app-create-listing',
   templateUrl: './create-listing.component.html',
-  styleUrls: ['./create-listing.component.css']
+  styleUrls: ['./create-listing.component.css'],
 })
 export class CreateListingComponent implements OnInit {
+  constructor(private createListingService: CreateListingService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleSubmitForm(value: {
+    item: string;
+    brand: string;
+    imageUrl: string;
+    description: string;
+    city: string;
+    country: string;
+    price: string;
+  }) {
+    this.createListingService.createListing(value).subscribe({
+      next: (response) => {
+        console.log(response);
+        //redirect to catalog or profile page
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
-
 }
