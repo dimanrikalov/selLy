c025c0c1414cbcb4ff8893ea8c734cb774641325
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-24 20:14:11 +0200

    Added validation for Edit listing page

--- client/app/src/app/edit-listing/edit-listing.component.ts
+++ client/app/src/app/edit-listing/edit-listing.component.ts
@@ -9,6 +9,7 @@ import { ListingOperationsService } from '../services/listing-operations.service
 })
 export class EditListingComponent implements OnInit {
 
+  errorMessage : string | null = null;
   listingId : string | null = null;
   loggedUserId: string | null = localStorage.getItem('userId');
 
@@ -34,10 +35,41 @@ export class EditListingComponent implements OnInit {
     country: string;
     price: string;
   }) {
-
     if(this.listingId === null) {
       return;
     }
+
+    if(value.item!.length < 2 || value.item.length > 20) {
+      this.errorMessage = 'Item length must be in range 2-20!'
+      return;
+    }
+
+    if(!value.imageUrl.startsWith('http://') && !value.imageUrl.startsWith('https://')) {
+      this.errorMessage = 'Invalid image URL! (Must start with http:// or https://)';
+      return;
+    }
+
+    if(value.description.length < 15) {
+      this.errorMessage = 'Item\'s description must be at least 15 characters long!';
+      return; 
+    }
+
+    if(!value.city.match('[A-Z][a-z]+')) {
+      this.errorMessage = 'City name must start with capital letter!';
+      return;
+    }
+
+    if(!value.country.match('[A-Z][a-z]+')) {
+      this.errorMessage = 'Country name must start with capital letter!';
+      return;
+    }
+
+    if(Number(value.price) < 0) {
+      this.errorMessage = 'Minimum price is 0.00 euros!';
+      return;
+    }
+
+    this.errorMessage = null;
 
     this.editListingService.editListing(value, this.listingId, this.loggedUserId!).subscribe({
       next: (response) => {
