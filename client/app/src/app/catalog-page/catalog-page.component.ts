268d66df829913d9323d19844373dd8ec238cd3f
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-24 22:31:51 +0200

    Added search functionality tо catalog page

--- client/app/src/app/catalog-page/catalog-page.component.ts
+++ client/app/src/app/catalog-page/catalog-page.component.ts
@@ -5,13 +5,12 @@ import { ListingsApiService } from '../services/listings-api.service';
 @Component({
   selector: 'app-catalog-page',
   templateUrl: './catalog-page.component.html',
-  styleUrls: ['./catalog-page.component.css']
+  styleUrls: ['./catalog-page.component.css'],
 })
 export class CatalogPageComponent implements OnInit {
-
   allListings: IListing[] | null = null;
 
-  constructor(private listingsApiService: ListingsApiService) { }
+  constructor(private listingsApiService: ListingsApiService) {}
 
   ngOnInit(): void {
     this.listingsApiService.loadListings().subscribe({
@@ -19,9 +18,35 @@ export class CatalogPageComponent implements OnInit {
         this.allListings = value;
       },
       error: (err) => {
-        console.error(err)
-      }
-    })
+        console.error(err);
+      },
+    });
   }
 
+  
+  handleFormSubmit(value: { searchInput: string; sortType: string }) {
+    console.log(value.sortType);
+    this.allListings =
+      this.allListings
+        ?.filter(
+          (x) =>
+            x.item.toLowerCase().includes(value.searchInput.toLowerCase()) ||
+            x.brand.toLowerCase().includes(value.searchInput.toLowerCase())
+        )
+        .sort((a, b): any => {
+          if (value.sortType === 'priceAscending') {
+            console.log('sort by priceAscending')
+            return a.price - b.price;
+          } else if (value.sortType === 'priceDescending') {
+            console.log('sort by priceDescending')
+            return b.price - a.price;
+          } else if (value.sortType === 'a-z') {
+            console.log('sort by a-z')
+            return a.item.localeCompare(b.item);
+          } else if (value.sortType === 'z-a') {
+            console.log('sort by z-a')
+            return b.item.localeCompare(a.item);
+          }
+        }) || null;
+  }
 }
