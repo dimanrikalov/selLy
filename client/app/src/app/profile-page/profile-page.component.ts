a4cbdf3c938489c9544ea593c96f8ce99e161611
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-24 22:32:07 +0200

    added search functionality to profile page

--- client/app/src/app/profile-page/profile-page.component.ts
+++ client/app/src/app/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
 import { IUser } from '../interfaces/User';
 import { Component, OnInit , HostListener} from '@angular/core';
 import { ProfileService } from '../services/profile.service';
+import { IListing } from '../interfaces/Listing';
 
 @Component({
   selector: 'app-profile-page',
@@ -11,13 +12,14 @@ export class ProfilePageComponent implements OnInit {
 
   user: IUser | null = null;
   displayWelcome: boolean = false;
+  userListings: IListing[] | null = null;
   innerWidth: any;
   constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
 
     this.innerWidth = window.innerWidth;
-    if(this.innerWidth < 1195) {
+    if(this.innerWidth <= 1195) {
       this.displayWelcome= true;
     }
     
@@ -33,12 +35,37 @@ export class ProfilePageComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onResize(event : any) {
-    if(event.target.innerWidth < 1195) {
+    if(event.target.innerWidth <= 1195) {
       this.displayWelcome = true;
     } else {
       this.displayWelcome = false;
     }
     this.innerWidth = window.innerWidth;
+  }
+
+  handleFormSubmit(value: { searchInput: string; sortType: string }) {
+    this.userListings =
+      this.user!.listings
+        ?.filter(
+          (x) =>
+            x.item.toLowerCase().includes(value.searchInput.toLowerCase()) ||
+            x.brand.toLowerCase().includes(value.searchInput.toLowerCase())
+        )
+        .sort((a, b): any => {
+          if (value.sortType === 'priceAscending') {
+            console.log('sort by priceAscending')
+            return a.price - b.price;
+          } else if (value.sortType === 'priceDescending') {
+            console.log('sort by priceDescending')
+            return b.price - a.price;
+          } else if (value.sortType === 'a-z') {
+            console.log('sort by a-z')
+            return a.item.localeCompare(b.item);
+          } else if (value.sortType === 'z-a') {
+            console.log('sort by z-a')
+            return b.item.localeCompare(a.item);
+          }
+        }) || null;
   }
 
 }
