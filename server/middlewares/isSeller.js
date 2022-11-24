dbb61aa8438c6bf0d92213288e6e52a09d58350e
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-24 12:30:28 +0200

    fixed is seller middleware to respond to the payload structure

--- server/middlewares/isSeller.js
+++ server/middlewares/isSeller.js
@@ -2,7 +2,7 @@ const api = require('../services/listingService');
 
 module.exports = async (req, res, next) => {
     const listing = await api.getById(req.params.listingId);
-    if (listing.seller.toString() !== req.body.userId.toString()) {
+    if (listing.userId.toString() !== req.body.userId) {
         return res
             .status(404)
             .json({ errorMessage: 'You must be the seller!' });
