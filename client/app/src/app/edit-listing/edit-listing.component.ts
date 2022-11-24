a4665f034b759bbec753e934786ce6f9ebcaeb53
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-24 12:30:48 +0200

    Made edit component functional

--- client/app/src/app/edit-listing/edit-listing.component.ts
+++ client/app/src/app/edit-listing/edit-listing.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ListingOperationsService } from '../services/listing-operations.service';
 
 @Component({
   selector: 'app-edit-listing',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditListingComponent implements OnInit {
 
-  constructor() { }
+  listingId : string | null = null;
+
+  constructor(
+    private editListingService: ListingOperationsService,
+    private activatedRoute: ActivatedRoute
+    ) { 
+      this.activatedRoute.params.subscribe((params) => {
+        this.listingId = params['id'];
+      });
+    }
 
   ngOnInit(): void {
+  }
+
+  handleSubmitForm(value: {
+    item: string;
+    brand: string;
+    imageUrl: string;
+    description: string;
+    city: string;
+    country: string;
+    price: string;
+  }) {
+
+    if(this.listingId === null) {
+      return;
+    }
+
+    this.editListingService.editListing(value, this.listingId).subscribe({
+      next: (response) => {
+        console.log(response);
+        //redirect to catalog or profile page
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
   }
 
 }
