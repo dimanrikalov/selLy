499435e32d17c4667485d839f19a49297a519089
Diman Rikalov <dimanrikalov1@gmail.com>
2022-11-25 13:35:53 +0200

    Added welcome page greeting functionality depending on if there is a logged in user

--- client/app/src/app/core/welcome-page/welcome-page.component.ts
+++ client/app/src/app/core/welcome-page/welcome-page.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.css']
+  styleUrls: ['./welcome-page.component.css'],
 })
 export class WelcomePageComponent implements OnInit {
+  userName: string | null = null;
 
-  constructor() { }
+  constructor(private profileService: ProfileService) {}
 
   ngOnInit(): void {
+    this.profileService.loadProfile().subscribe({
+      next: (user) => {
+        this.userName = `, ${user.name
+          .slice(0, user.name.indexOf(' '))}`;
+      },
+      error: (err) => {
+        this.userName = ' to Selly';
+        console.log(err);
+      },
+    });
   }
-
 }
+
